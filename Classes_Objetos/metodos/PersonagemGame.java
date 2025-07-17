38ccf44275f073954a341016d0005fefec1de45e
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-17 20:01:32 -0300

    Status do Personagem

--- Classes_Objetos/metodos/PersonagemGame.java
+++ Classes_Objetos/metodos/PersonagemGame.java
@@ -1,34 +1,34 @@
-public class PersonagemGame {
-
-	private static final int ZERO = 0;
-	private static final int CEM = 100;
-	private String nome;
-	private int saudeAtual;
-	public int quantidadeDeDano;
-	public int quantidadeDeCura;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getSaudeAtual() {
-		return saudeAtual;
-	}
-	public void setSaudeAtual(int saudeAtual) {
-		this.saudeAtual = saudeAtual;
-	}
-	public void tomarDano(int quantidadeDeDano) {
-		this.saudeAtual = this.saudeAtual-quantidadeDeDano; 			
-		if(this.saudeAtual<ZERO) {
-			this.saudeAtual=ZERO;
-		}
-	}
-	public void receberCura(int quantidadeDeCura) {
-		this.saudeAtual = this.saudeAtual+quantidadeDeCura; 			
-		if(this.saudeAtual>CEM) {
-			this.saudeAtual=CEM;
-		}
-	}
-}
+//public class PersonagemGame {
+//
+//	private static final int ZERO = 0;
+//	private static final int CEM = 100;
+//	private String nome;
+//	private int saudeAtual;
+//	public int quantidadeDeDano;
+//	public int quantidadeDeCura;
+//	
+//	public String getNome() {
+//		return nome;
+//	}
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+//	public int getSaudeAtual() {
+//		return saudeAtual;
+//	}
+//	public void setSaudeAtual(int saudeAtual) {
+//		this.saudeAtual = saudeAtual;
+//	}
+//	public void tomarDano(int quantidadeDeDano) {
+//		this.saudeAtual = this.saudeAtual-quantidadeDeDano; 			
+//		if(this.saudeAtual<ZERO) {
+//			this.saudeAtual=ZERO;
+//		}
+//	}
+//	public void receberCura(int quantidadeDeCura) {
+//		this.saudeAtual = this.saudeAtual+quantidadeDeCura; 			
+//		if(this.saudeAtual>CEM) {
+//			this.saudeAtual=CEM;
+//		}
+//	}
+//}
