583ec123db179b838b209c85827c989e6539df6a
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-17 20:13:18 -0300

    Consistência

---
+++ Classes_Objetos/consistencia/PersonagemGame.java
@@ -0,0 +1,58 @@
+public class PersonagemGame {
+
+	private static final int ZERO = 0;
+	private static final int CEM = 100;
+	private String nome;
+	private int saudeAtual;
+	public int quantidadeDeDano;
+	public int quantidadeDeCura;
+	private String status;
+	
+	public PersonagemGame(int saudeAtual, String nome) {
+		super();
+		this.nome = nome;
+		setSaudeAtual(saudeAtual);
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome == null || nome.isBlank()) {
+			return;
+		}
+		this.nome = nome;
+	}
+	
+	public int getSaudeAtual() {
+		return saudeAtual;
+	}
+	public void setSaudeAtual(int saudeAtual) {
+		this.saudeAtual = saudeAtual;
+		if(ZERO == this.saudeAtual) {
+			this.status="morto";
+		}else {
+			this.status="vivo";
+		}
+	}
+	
+	public void tomarDano(int quantidadeDeDano) {
+		int saude = this.saudeAtual-quantidadeDeDano; 			
+		if(saude < ZERO) {
+			saude = ZERO;
+		}
+		setSaudeAtual(saude);
+	}
+	public void receberCura(int quantidadeDeCura) {
+		int saude = this.saudeAtual+quantidadeDeCura; 			
+		if(saude > CEM) {
+			saude = CEM;
+		}
+		setSaudeAtual(saude);
+	}
+	
+	public String getStatus() {
+		return status;
+	}
+
+}
--- Classes_Objetos/status/PersonagemGame.java
+++ Classes_Objetos/status/PersonagemGame.java
@@ -1,49 +1,49 @@
-public class PersonagemGame {
-
-	private static final int ZERO = 0;
-	private static final int CEM = 100;
-	private String nome;
-	private int saudeAtual;
-	public int quantidadeDeDano;
-	public int quantidadeDeCura;
-	private String status;
-	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public int getSaudeAtual() {
-		return saudeAtual;
-	}
-	public void setSaudeAtual(int saudeAtual) {
-		this.saudeAtual = saudeAtual;
-		if(ZERO == this.saudeAtual) {
-			this.status="morto";
-		}else {
-			this.status="vivo";
-		}
-	}
-	
-	public void tomarDano(int quantidadeDeDano) {
-		int saude = this.saudeAtual-quantidadeDeDano; 			
-		if(saude < ZERO) {
-			saude = ZERO;
-		}
-		setSaudeAtual(saude);
-	}
-	public void receberCura(int quantidadeDeCura) {
-		int saude = this.saudeAtual+quantidadeDeCura; 			
-		if(saude > CEM) {
-			saude = CEM;
-		}
-		setSaudeAtual(saude);
-	}
-	
-	public String getStatus() {
-		return status;
-	}
-
-}
+//public class PersonagemGame {
+//
+//	private static final int ZERO = 0;
+//	private static final int CEM = 100;
+//	private String nome;
+//	private int saudeAtual;
+//	public int quantidadeDeDano;
+//	public int quantidadeDeCura;
+//	private String status;
+//	
+//	public String getNome() {
+//		return nome;
+//	}
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+//	
+//	public int getSaudeAtual() {
+//		return saudeAtual;
+//	}
+//	public void setSaudeAtual(int saudeAtual) {
+//		this.saudeAtual = saudeAtual;
+//		if(ZERO == this.saudeAtual) {
+//			this.status="morto";
+//		}else {
+//			this.status="vivo";
+//		}
+//	}
+//	
+//	public void tomarDano(int quantidadeDeDano) {
+//		int saude = this.saudeAtual-quantidadeDeDano; 			
+//		if(saude < ZERO) {
+//			saude = ZERO;
+//		}
+//		setSaudeAtual(saude);
+//	}
+//	public void receberCura(int quantidadeDeCura) {
+//		int saude = this.saudeAtual+quantidadeDeCura; 			
+//		if(saude > CEM) {
+//			saude = CEM;
+//		}
+//		setSaudeAtual(saude);
+//	}
+//	
+//	public String getStatus() {
+//		return status;
+//	}
+//
+//}
