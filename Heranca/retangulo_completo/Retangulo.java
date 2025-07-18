39818b6234fd9406310e5ff749cddd3fd0aced8d
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-17 22:05:31 -0300

    Retângulo Completo

---
+++ Heranca/retangulo_completo/Retangulo.java
@@ -0,0 +1,42 @@
+
+public class Retangulo extends FormaGeometrica{
+
+	private double largura;
+	private double altura;
+	
+	@Override
+	public double area() throws UnsupportedOperationException {
+		return largura*altura;
+	}
+	
+	public double getLargura() {
+		return largura;
+	}
+	
+	public void setLargura(double largura) throws IllegalArgumentException {
+		if(largura<0){
+			throw new IllegalArgumentException("Largura deve ser maior ou igual a 0");
+		}else {
+			this.largura = largura;			
+		}
+	}
+	
+	public double getAltura() {
+		return altura;
+	}
+	
+	public void setAltura(double altura) {
+		if(altura<0){
+			throw new IllegalArgumentException("Altura deve ser maior ou igual a 0");
+		}else {
+			this.altura = altura;			
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "[Retangulo] "+ largura + " / " + altura;
+	}
+
+	
+}
