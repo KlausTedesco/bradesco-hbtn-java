863493cd8560f71fb5a4621b138d7fde1c99eb3a
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-17 22:17:43 -0300

    Comentar retangulo completp

--- Heranca/retangulo_completo/FormaGeometrica.java
+++ Heranca/retangulo_completo/FormaGeometrica.java
@@ -1,8 +1,8 @@
-
-public class FormaGeometrica {
-	
-	public double area() throws UnsupportedOperationException {
-			throw new UnsupportedOperationException("Funcao area nao implementada");			
-	}
-
-}
+//
+//public class FormaGeometrica {
+//	
+//	public double area() throws UnsupportedOperationException {
+//			throw new UnsupportedOperationException("Funcao area nao implementada");			
+//	}
+//
+//}
