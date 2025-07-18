363e2ae87891d5245ad41ed63a83906778edecf7
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-17 21:25:01 -0300

    Salário

---
+++ Heranca/salario/Gerente.java
@@ -0,0 +1,17 @@
+
+public class Gerente extends Empregado{
+
+	public Gerente(double salarioFixo) {
+		super(salarioFixo);
+	}
+
+	@Override
+	public double calcularBonus(Departamento departamento) {
+		// retorna o valor bônus do empregado conforme dados do empregado e do departamento.
+		if(departamento.alcancouMeta()) {
+			double percentual = 0.20;
+			return getSalarioFixo()*percentual;
+		}		
+		return 0;
+	}
+}
