b32152433882dacf0cc90bc077fc498a1510ddc6
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-18 20:27:32 -0300

    Resumo Pedido

--- Heranca_Exercicios/pedido_livraria/Pedido.java
+++ Heranca_Exercicios/pedido_livraria/Pedido.java
@@ -1,3 +1,5 @@
+import java.text.DecimalFormat;
+
 public class Pedido {
 
 	private double percentualDesconto;
@@ -15,12 +17,57 @@ public class Pedido {
 		//deve-se também aplicar o percentual de desconto ao total dos itens.
 		double valorTotal = 0; 	
 		for (ItemPedido item : itens) {
-            double valor = item.getProduto().obterPrecoLiquido()*item.getQuantidade();
+            double valor = calcularValorPelaQuantidadeItem(item);
             valorTotal+=valor;
         }
 		
-		return valorTotal*(1-(percentualDesconto/100));
+		return aplicarDescontoTotal(valorTotal);
 		
+	}
+
+	private double aplicarDescontoTotal(double valorTotal) {
+		return valorTotal*(1-(percentualDesconto/100));
+	}
+
+	private double totalDesconto(double valorTotal) {
+		return valorTotal*(percentualDesconto/100);
+	}
+	
+	private double calcularValorPelaQuantidadeItem(ItemPedido item) {
+		return item.getProduto().obterPrecoLiquido()*item.getQuantidade();
+	}
+	
+	public void apresentarResumoPedido(){
+//		------- RESUMO PEDIDO -------
+//		Tipo: Livro  Titulo: Um de nos esta mentindo  Preco: 40,17  Quant: 1  Total: 40,17
+//		Tipo: Livro  Titulo: Mindset Milionario  Preco: 36,46  Quant: 2  Total: 72,91
+//		Tipo: Dvd  Titulo: Anjos da Noite 5 - Guerras de Sangue  Preco: 20,28  Quant: 1  Total: 20,28
+//		----------------------------
+//		DESCONTO: 6,67
+//		TOTAL PRODUTOS: 133,36
+//		----------------------------
+//		TOTAL PEDIDO: 126,69
+//		----------------------------
+		double valorTotal = 0; 	
+		System.out.println("------- RESUMO PEDIDO -------");
+		for (ItemPedido item : itens) {
+            System.out.println("Tipo: "+item.getProduto().getClass().getSimpleName()+"  Titulo: "+item.getProduto().getTitulo()
+            		+"  Preco: "+formatarDuasCasasDecimais(item.getProduto().obterPrecoLiquido())+"  Quant: "+item.getQuantidade()
+            		+"  Total: "+formatarDuasCasasDecimais(calcularValorPelaQuantidadeItem(item)));
+            valorTotal+=calcularValorPelaQuantidadeItem(item);
+        }
+		System.out.println("----------------------------");
+		System.out.println("DESCONTO: "+formatarDuasCasasDecimais(totalDesconto(valorTotal)));
+		System.out.println("TOTAL PRODUTOS: "+formatarDuasCasasDecimais(valorTotal));
+		System.out.println("----------------------------");
+		System.out.println("TOTAL PEDIDO: "+formatarDuasCasasDecimais(aplicarDescontoTotal(valorTotal)));
+		System.out.println("----------------------------");
+
+	}
+
+	private String formatarDuasCasasDecimais(double variavel) {
+		DecimalFormat dfa = new DecimalFormat("0.00");
+		return dfa.format(variavel);
 	}
 
 	public double getPercentualDesconto() {
