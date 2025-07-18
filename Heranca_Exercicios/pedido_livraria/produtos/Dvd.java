0de94b1758a3c0ef8bffcdd99fe71b679b21e2d3
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-18 20:33:08 -0300

    Comentar

--- Heranca_Exercicios/pedido_livraria/produtos/Dvd.java
+++ Heranca_Exercicios/pedido_livraria/produtos/Dvd.java
@@ -1,35 +1,35 @@
-package produtos;
-
-public class Dvd extends Produto{
-
-	private String diretor;
-	private String genero;
-	private int duracao;
-	
-	public Dvd(String titulo, int ano, String pais, double precoBruto, String diretor, String genero, int duracao) {
-		super(titulo, ano, pais, precoBruto);
-		this.diretor = diretor;
-		this.genero = genero;
-		this.duracao = duracao;
-	}
-
-	@Override
-	public double obterPrecoLiquido() {
-		// //DVDs: o cálculo de seu preço líquido é seu preço bruto com acréscimo de 20%
-		double acrescimo = 1.20;
-		return precoBruto*acrescimo;
-	}
-
-	public String getDiretor() {
-		return diretor;
-	}
-
-	public String getGenero() {
-		return genero;
-	}
-
-	public int getDuracao() {
-		return duracao;
-	}
-
-}
+//package produtos;
+//
+//public class Dvd extends Produto{
+//
+//	private String diretor;
+//	private String genero;
+//	private int duracao;
+//	
+//	public Dvd(String titulo, int ano, String pais, double precoBruto, String diretor, String genero, int duracao) {
+//		super(titulo, ano, pais, precoBruto);
+//		this.diretor = diretor;
+//		this.genero = genero;
+//		this.duracao = duracao;
+//	}
+//
+//	@Override
+//	public double obterPrecoLiquido() {
+//		// //DVDs: o cálculo de seu preço líquido é seu preço bruto com acréscimo de 20%
+//		double acrescimo = 1.20;
+//		return precoBruto*acrescimo;
+//	}
+//
+//	public String getDiretor() {
+//		return diretor;
+//	}
+//
+//	public String getGenero() {
+//		return genero;
+//	}
+//
+//	public int getDuracao() {
+//		return duracao;
+//	}
+//
+//}
