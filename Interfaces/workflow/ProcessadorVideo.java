be33d9be1f5eb1c8561d22f5bfd5018248bbc467
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-21 18:45:36 -0300

    Logging

---
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class ProcessadorVideo {
+	ArrayList<CanalNotificacao> canais = new ArrayList<CanalNotificacao>();
+	
+	void registrarCanal(CanalNotificacao canal){
+		//Add canal a lista de canais
+		this.canais.addLast(canal);
+	}
+	
+	void processar(Video video) {
+		//Esse método deve percorrer por todos os canais registros 
+		//e notificar a mensagem <arquivo> - <formato> do tipo LOG
+		for(CanalNotificacao canal : this.canais) {
+			String mensagem = video.arquivo+" - "+video.formatoVideo;
+			
+			canal.notificar(new Mensagem(mensagem, TipoMensagem.LOG));
+		}
+	}
+}
---
+++ Interfaces/workflow/Video.java
@@ -0,0 +1,13 @@
+
+public class Video {
+
+	String arquivo;
+	FormatoVideo formatoVideo;
+
+	public Video(String arquivo, FormatoVideo formatoVideo) {
+		super();
+		this.arquivo = arquivo;
+		this.formatoVideo = formatoVideo;
+	}
+
+}
