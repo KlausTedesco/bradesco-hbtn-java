ec2de6fb247d7595e1e049f7f4880fd0618a9f15
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-22 17:30:48 -0300

    Workflow

---
+++ Interfaces_Exercicios/workflow/MaquinaWorkflow.java
@@ -0,0 +1,11 @@
+import atividades.Atividade;
+
+public class MaquinaWorkflow {
+	
+	void executar(Workflow workflow) {
+		for(Atividade atividade: workflow.listaAtividades) {
+			atividade.executar();
+		}
+	}
+
+}
---
+++ Interfaces_Exercicios/workflow/Workflow.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+import atividades.Atividade;
+
+public class Workflow {
+	
+	ArrayList<Atividade> listaAtividades = new ArrayList<Atividade>();
+
+	void registrarAtividade(Atividade atividade) {
+		listaAtividades.add(atividade);
+	}
+}
