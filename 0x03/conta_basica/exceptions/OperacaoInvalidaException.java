c50daf8ef196a29ad7e0703e47d206658dd9ff3c
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-30 14:10:14 -0300

    Conta Bancária Básica

---
+++ 0x03/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception{
+
+	private static final long serialVersionUID = 3656629272828333103L;
+
+	public OperacaoInvalidaException(String message) {
+		super(message);
+	}
+	
+}
