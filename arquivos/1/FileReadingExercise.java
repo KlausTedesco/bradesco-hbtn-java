228b82d7618094fc8b574089428554118047dd44
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-07-31 17:02:36 -0300

    Ajuste no path

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -7,17 +8,27 @@ public class FileReadingExercise {
     public static void main(String[] args) {
         String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("/bradesco-hbtn-java/arquivos/1/"+fileName))){
+        try (BufferedReader reader = new BufferedReader(new FileReader("arquivos/1/"+fileName))){
         	String line = reader.readLine();
         	
         	while(line != null) {
         		System.out.println(line);
         		line = reader.readLine();
         	}
-			
-        	System.out.println("\nLeitura do arquivo concluída.");
-		} catch (Exception e) {
-			System.out.println("Houve algo de errado com a leitura do arquivo!");
-		} 
+			System.out.println("Leitura do arquivo concluída.");
+        
+        } catch (FileNotFoundException e) {
+            System.err.println("Erro: Arquivo não encontrado: " + e.getMessage());
+            
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        
+        } catch (SecurityException e) {
+            System.err.println("Erro de segurança: " + e.getMessage());       
+        
+        } catch (Exception e) {
+			System.err.println("Houve algo de errado com a leitura do arquivo!");
+		
+        } 
     }
 }
