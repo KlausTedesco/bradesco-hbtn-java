ce989a39cf5a85dbe6b44c236c044b600ef34c83
Klaus <klaus.tedesco@gmail.com>
2025-08-01 14:12:43 +0000

    Comentar

--- arquivos/1/FileReadingExercise.java
+++ arquivos/1/FileReadingExercise.java
@@ -1,36 +1,36 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+// import java.io.BufferedReader;
+// import java.io.FileNotFoundException;
+// import java.io.FileReader;
+// import java.io.IOException;
 
 
-public class FileReadingExercise {
-    public static void main(String[] args) {
-        String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
+// public class FileReadingExercise {
+//     public static void main(String[] args) {
+//         String fileName = "exemplo.txt"; // Nome do arquivo fixo para leitura
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
+//         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))){
             
-            System.out.println("Conteúdo do arquivo '" + fileName + "':\n");
-        	String line = reader.readLine();
+//             System.out.println("Conteúdo do arquivo '" + fileName + "':\n");
+//         	String line = reader.readLine();
         	
-        	while(line != null) {
-        		System.out.println(line);
-        		line = reader.readLine();
-        	}
-			System.out.println("\nLeitura do arquivo concluída.");
+//         	while(line != null) {
+//         		System.out.println(line);
+//         		line = reader.readLine();
+//         	}
+// 			System.out.println("\nLeitura do arquivo concluída.");
         
-        } catch (FileNotFoundException e) {
-            System.err.println("Erro: Arquivo não encontrado: " + e.getMessage());
+//         } catch (FileNotFoundException e) {
+//             System.err.println("Erro: Arquivo não encontrado: " + e.getMessage());
             
-        } catch (IOException e) {
-            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+//         } catch (IOException e) {
+//             System.err.println("Erro ao ler o arquivo: " + e.getMessage());
         
-        } catch (SecurityException e) {
-            System.err.println("Erro de segurança: " + e.getMessage());       
+//         } catch (SecurityException e) {
+//             System.err.println("Erro de segurança: " + e.getMessage());       
         
-        } catch (Exception e) {
-			System.err.println("Houve algo de errado com a leitura do arquivo!");
+//         } catch (Exception e) {
+// 			System.err.println("Houve algo de errado com a leitura do arquivo!");
 		
-        } 
-    }
-}
+//         } 
+//     }
+// }
