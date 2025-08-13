a01d845ab7755458da241ef543f0b662c25ae113
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-08-13 10:36:28 -0300

    Complexidade O(1) - Tempo Constante

---
+++ complexidade/0/PessoasArray.java
@@ -0,0 +1,26 @@
+
+public class PessoasArray {
+
+    private String[] nomes;
+
+    public PessoasArray() {
+        nomes =new String[]{"Amanda", "Beatriz", "Carlos","Daniela","Eduardo",
+                "Fabio","Gustavo", "Hingrid","Isabela","Joao","Leise","Maria",
+                "Norberto","Otavio","Paulo", "Quirino","Renata","Sabata",
+                "Tais","Umberto","Vanessa","Xavier"};
+    }
+
+    public String[] getNomes() {
+        return nomes;
+    }
+
+    public void setNomes(String[] nomes) {
+        this.nomes = nomes;
+    }
+
+    public void pesquisaTempoConstante(int id){
+    		try { System.out.println("Nome pesquisado é "+ this.nomes[id] +" que está na posição "+id);			
+			} catch (Exception e) {throw new IllegalArgumentException("O array de nomes possui :"+ this.nomes.length +" nomes.");}
+    }
+	
+}
