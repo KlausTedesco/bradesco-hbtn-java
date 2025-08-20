981f318a007d8db5a661b5cea20be583b1f071a6
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-08-20 09:59:59 -0300

    Generics Mídias

---
+++ Generics/midias/Biblioteca.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Biblioteca<T>{
+
+	private List<T> lista;
+	
+	public Biblioteca() {
+		this.lista = new ArrayList<>();
+	}
+
+	public void adicionarMidia(T midia) {
+		this.lista.add(midia);
+	}
+	
+	public String obterListaMidias() {
+		return this.lista.toString();
+	}
+}
---
+++ Generics/midias/Midia.java
@@ -0,0 +1,20 @@
+
+public class Midia {
+	
+	private String nome;
+	
+	public Midia(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Tipo: %s - Nome: %s", this.getClass().getName(), getNome()); 
+	}
+	
+}
