96aca7e95e23c7bab1d54cdb997cd5447f749674
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-03 07:54:51 -0300

    Relatórios Livros

---
+++ Streams_Collectors_Optional/livros/Produto.java
@@ -0,0 +1,38 @@
+public class Produto {
+
+	private int codigo;
+	private String nome;
+	private CategoriaProduto categoriaProduto;
+	private double preco;
+	
+	public Produto(int codigo, String nome, CategoriaProduto categoriaProduto, double preco) {
+		super();
+		this.codigo = codigo;
+		this.nome = nome;
+		this.categoriaProduto = categoriaProduto;
+		this.preco = preco;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public CategoriaProduto getCategoriaProduto() {
+		return categoriaProduto;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[" + codigo + "] " + nome + " " + categoriaProduto + " R$ %.2f", preco);
+	}
+	
+	
+}
