ffc80849df1bfd355c8706aa2810f63d6e59d071
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-03 17:11:28 -0300

    Relatório Produto com maior preço

---
+++ Streams_Collectors_Optional/maior_preco/Consulta.java
@@ -0,0 +1,29 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido){
+		List<Produto> listaDeProdutos = pedido.getProdutos();
+		
+		List<Produto> listaDeLivros = listaDeProdutos.stream()
+				.filter(p -> p.getCategoriaProduto().equals(CategoriaProduto.LIVRO))
+				.collect(Collectors.toList());
+		
+		return listaDeLivros;
+	}
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+		Produto produtoMaiorPreco = null;
+		Optional<Produto> produtoOptional = produtos.stream()
+				.max(Comparator.comparingDouble(Produto::getPreco));
+		
+		if(produtoOptional.isPresent()) {
+			produtoMaiorPreco = produtoOptional.get();
+		}
+		
+		return produtoMaiorPreco;
+	}
+}
