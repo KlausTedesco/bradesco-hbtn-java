9f694a4751c4d1790d010918ccbe1d9dd2de4db1
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-04 15:26:33 -0300

    Relatório Pedidos com Eletrônicos

---
+++ Streams_Collectors_Optional/pedidos_eletronico/Consulta.java
@@ -0,0 +1,45 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class Consulta {
+
+	public static List<Produto> obterLivrosDoPedido(Pedido pedido) {
+		List<Produto> listaDeProdutos = pedido.getProdutos();
+
+		List<Produto> listaDeLivros = listaDeProdutos.stream()
+				.filter(p -> p.getCategoriaProduto().equals(CategoriaProduto.LIVRO)).collect(Collectors.toList());
+
+		return listaDeLivros;
+	}
+
+	public static Produto obterProdutoMaiorPreco(List<Produto> produtos) {
+		Produto produtoMaiorPreco = null;
+		Optional<Produto> produtoOptional = produtos.stream().max(Comparator.comparingDouble(Produto::getPreco));
+
+		if (produtoOptional.isPresent()) {
+			produtoMaiorPreco = produtoOptional.get();
+		}
+
+		return produtoMaiorPreco;
+	}
+
+	public static List<Produto> obterProdutosPorPrecoMinimo(List<Produto> produtos, double precoMinimo) {
+		List<Produto> listaProdutosFiltrado = produtos.stream().filter(p -> p.getPreco() >= precoMinimo)
+				.collect(Collectors.toList());
+
+		return listaProdutosFiltrado;
+	}
+
+	public static List<Pedido> obterPedidosComEletronicos(List<Pedido> pedidos) {			
+		List<Pedido> listaPedidosFiltrado = pedidos.stream()
+				.filter(pedido -> pedido.getProdutos().stream()
+						.anyMatch(produto -> produto.getCategoriaProduto().equals(CategoriaProduto.ELETRONICO))) 
+//				.flatMap(pedido -> pedido.getProdutos().stream()) 
+	//			.filter(produto -> produto.getCategoriaProduto().equals(CategoriaProduto.ELETRONICO)) 
+				.collect(Collectors.toList()); 
+		
+		return listaPedidosFiltrado;
+	}
+}
