07158d210b5f3c090c79b3964ff37a09c2a1f5f9
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-18 09:24:00 -0300

    Contagem de pessoas por idade e cargo

---
+++ Streams_Collector_Optional_Exercicios/quantidade_pessoas_por_cargo_idade/Pessoa.java
@@ -0,0 +1,48 @@
+public class Pessoa implements Comparable<Pessoa>{
+	
+	private int codigo;
+	private String nome;
+	private String cargo;
+	private int idade;
+	private double salario;
+	
+	public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+		this.codigo = codigo;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.idade = idade;
+		this.salario = salario;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[" + codigo + "] " + nome + " " + cargo + " " + idade + " R$ %.6f", salario)
+		.replace(".", ",");
+	}
+
+	@Override
+	public int compareTo(Pessoa outraPessoa) {
+        return this.nome.compareTo(outraPessoa.nome);
+	}
+	
+}
