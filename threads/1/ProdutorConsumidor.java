3bc13cbb11a3cfb61b7ffae158a967901a335665
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-23 17:00:27 -0300

    Notificações em Java

---
+++ threads/1/ProdutorConsumidor.java
@@ -0,0 +1,16 @@
+public class ProdutorConsumidor {
+	
+	public static void main(String[] args) throws InterruptedException {
+		Buffer buffer = new Buffer();
+
+		// Criar e iniciar as threads do produtor e consumidor
+		Thread consumidor = new Consumidor(buffer);
+		Thread produtor = new Produtor(buffer);
+		produtor.start();
+		consumidor.start();
+
+		produtor.join();
+		consumidor.join();
+	}
+	
+}
