08f98d565e4b1dd9ad8035decd1dce40ef6d1d29
Klaus Tedesco <klaus.tedesco@gmail.com>
2025-09-29 15:11:16 -0300

    Ajuste validação nome da variavel "random"

--- threads/2/Produtor.java
+++ threads/2/Produtor.java
@@ -3,18 +3,18 @@ import java.util.Random;
 public class Produtor extends Thread {
   
 	private Fila fila;
-	private Random gerador;
+	private Random random;
 	
     public Produtor(Fila fila) {
     	this.fila = fila;
-    	this.gerador = new Random();
+    	this.random = new Random();
 	}
 
 	@Override
     public void run() {
         try {
         	while (true) {            			
-        		int num = this.gerador.nextInt(100);
+        		int num = this.random.nextInt(100);
         		Thread.sleep(1000);    
             	this.fila.adicionar(num);
         	}
